131858c566b5ba7be79e741c94dcb2695e195388
vinutahut <hutagikarvinuta@gmail.com>
2024-06-30 23:38:38 +0530

    Updates

---
+++ precache-manifest.a7e9812ea172770198e94ce2ffe3b878.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f329f7d8a4c25e263870b7b235cbce49",
+    "url": "/smartcart/index.html"
+  },
+  {
+    "revision": "e3599fd98fc24411c4d9",
+    "url": "/smartcart/static/css/main.b6948965.chunk.css"
+  },
+  {
+    "revision": "a171a33cbeedfa3054c2",
+    "url": "/smartcart/static/js/2.8186b509.chunk.js"
+  },
+  {
+    "revision": "45368973031116a9f94f836e8a3d931c",
+    "url": "/smartcart/static/js/2.8186b509.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeab0cbdbbbb8d74a48f",
+    "url": "/smartcart/static/js/3.cb447600.chunk.js"
+  },
+  {
+    "revision": "e3599fd98fc24411c4d9",
+    "url": "/smartcart/static/js/main.6a795c33.chunk.js"
+  },
+  {
+    "revision": "faef4e000d63e109e6b4",
+    "url": "/smartcart/static/js/runtime-main.d287e6f7.js"
+  }
+]);
